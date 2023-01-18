40dd4621e7c2fe2ad00ac66359e3e7416703d432
hidemaru-jp <111981295+hidemaru-jp@users.noreply.github.com>
2023-01-18 14:57:46 +0900

    [ADD]studies_validaties & studies_error_messages & time_to_ja

--- app/models/study.rb
+++ app/models/study.rb
@@ -1,2 +1,7 @@
 class Study < ApplicationRecord
+  validates :title , length: {minimum: 1, maximum: 20 }
+  validates :content, length: {minimum: 1, maximum: 100}
+  validates :date, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
 end
